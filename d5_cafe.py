601af3c14a364f0976c433f794438b52a34ba4a6
tooOld2Code <62121320+tooOld2Code@users.noreply.github.com>
2025-12-09 19:06:24 -0800

    day 5 tidy code for posting

--- d5_cafe.py
+++ d5_cafe.py
@@ -1,25 +1,9 @@
 # Day 5: Cafeteria
 # /learn/aoc_2025/d5_cafe.py
-import numpy as np
-# import pandas as pd
+# import numpy as np
 
 fl_pzl_input = '../aoc_2025/d5_food.txt'
 fl_tst_input = '../aoc_2025/d5_tst.txt'
-
-# with open(fl_pzl_input, "r") as f:
-#     d5_vals = [line.strip() for line in f]
-
-
-# d5_tst = """
-# """
-# tst_vals = d5_tst.splitlines()
-
-
-def inp_to_numpy(dD_in):
-  rdgs = [[int(bd) for bd in rdg.strip()] for rdg in dD_in]
-  r_np = np.array(rdgs)
-  return r_np
-
 
 # Change to False when finished dev and testing to run on puzzle 
 use_test = False
@@ -27,14 +11,6 @@ if use_test:
   curr_fl = fl_tst_input
 else:
   curr_fl = fl_pzl_input
-
-# cnt_rdgs = len(curr_inp)
-# nbr_dgts = len(curr_rdgs[0])
-
-# convert data to numpy array if necessary, using function above
-# np_d5 = inp_to_numpy(curr_inp)
-# or convert data to numeric if necessary, note this is done by pwr_to_numpy() if you are using it
-# d5_rdgs = [[int(bd) for bd in rdg] for rdg in d5_vals]
 
 # modify as needed for current input
 fresh = []
@@ -51,12 +27,6 @@ with open(curr_fl, "r") as f:
       avail.append(int(line.strip()))
 
 fresh.sort()
-
-if use_test:
-  print(fresh, "\n", avail)
-# else:
-#   l_rngs = len(fresh)
-#   print(fresh[0], fresh[l_rngs//4], fresh[l_rngs//2], fresh[(l_rngs*3)//4], fresh[-1])
 
 # part 1
 def part_1():
